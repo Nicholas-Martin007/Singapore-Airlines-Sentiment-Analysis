94d5ed42513218b1695850fb40bcca7db7349e5b
Nicholas-Martin007 <nicholas.martin03@yahoo.com>
2024-04-07 18:44:37 +0700

    Updated tokenization + Added split_data function + implemented DataLoader with a sampler for efficient data loading

--- dataLoader.py
+++ dataLoader.py
@@ -1,15 +1,27 @@
 import re
 from nltk.corpus import wordnet
 import string
+import pandas as pd
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from nltk.corpus import wordnet
 
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
 import torchtext
+import numpy as np
+
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import DataLoader
 
 def clean_data(df):
+    '''
+    Removing unnecessary data columns for training, then saved the data
+    '''
+
     drop_columns = ['published_date', 'published_platform', 'type', 'title', 'helpful_votes']
 
     df.drop(columns=drop_columns, inplace=True)
@@ -20,6 +32,10 @@ def clean_data(df):
     df.to_csv('data_clean.csv', index=False)
 
 def remove_emoji(text):
+    '''
+    Removing Emojis
+    '''
+
     emojis = re.compile("["
                     u"\U0001F600-\U0001F64F"
                     u"\U0001F300-\U0001F5FF"
@@ -51,37 +67,84 @@ def get_pos(tag):
     return dictionary.get(tag[0], wordnet.NOUN)
 
 
-def tokenization(paragraphs, glove, lowercase=True, stop_words=True):
+def retrieve_data(data, glove, lowercase=True, stop_words=True):
     '''
     Tokenize for 1 Paragraph
     '''
     tokenized_data = []
-    for paragraph in paragraphs:
-        if lowercase: paragraph = paragraph.lower()
-        # Remove Emoji
-        paragraph = remove_emoji(paragraph)
+    paragraphs = data['text']
 
-        tokens = paragraph.split()
-        # Remove Punctuation
-        paragraph = paragraph.translate(str.maketrans('', '', string.punctuation))
-        # Remove Stop Words
-        if stop_words:
-            stops = set(stopwords.words('english'))
-            tokens = [word for word in tokens if word not in stops]
-        
-        # Lemmatization
-        lemmatizer = WordNetLemmatizer()
-        
-        tokens = [lemmatizer.lemmatize(word=token, pos=get_pos(tag)) for token, tag in pos_tag(tokens)]
+    def tokenization():
+        for paragraph in paragraphs:
+            if lowercase:
+                paragraph = paragraph.lower()
 
-        tokenized_paragraph = []
-        for token in tokens:
-            if token and token in glove.stoi:
-                tokenized_paragraph.append(glove.stoi[token])
-            else:
-                tokenized_paragraph.append(glove.stoi['unk'])
+            # Remove words
+            paragraph = remove_emoji(paragraph)
 
-        tokenized_data.append(tokenized_paragraph)
+            # Split paragraph into bunch of words
+            tokens = paragraph.split()
 
-    return tokenized_data
+            # Removing punctuation
+            paragraph = paragraph.translate(str.maketrans('', '', string.punctuation))
+
+            # Removing stop words
+            if stop_words:
+                stops = set(stopwords.words('english'))
+                tokens = [word for word in tokens if word not in stops]
+            
+            # Lemmatization
+            lemmatizer = WordNetLemmatizer()
+            tokens = [lemmatizer.lemmatize(word=token, pos=get_pos(tag)) for token, tag in pos_tag(tokens)]
+
+            # Inserting into glove word embeddings
+            tokenized_paragraph = []
+            for token in tokens:
+                if token and token in glove.stoi:
+                    tokenized_paragraph.append(glove.stoi[token])
+                else:
+                    tokenized_paragraph.append(glove.stoi['unk'])
+
+            # Truncate + Padding
+            if len(tokenized_paragraph) < 256:
+                tokenized_paragraph += [glove.stoi['pad']] * (256 - len(tokenized_paragraph))
+            elif len(tokenized_paragraph) > 256:
+                tokenized_paragraph = tokenized_paragraph[:256]
+
+            tokenized_data.append(tokenized_paragraph)
+
+    tokenization()
+
+    # Yang d.items() gimana buat?
+
+    return list(zip(tokenized_data, data['rating']))
+
+
+def split_data(data, val_size, test_size):
+    split_val = int(val_size * len(data))
+    split_test = int(test_size * len(data))
+
+    train_data, val_temp = data[split_val:], data[:split_val]
+    val_data, test_data = val_temp[split_test:], val_temp[:split_test]
+
+
+    return train_data, val_data, test_data
+
+
+def getDataLoader(data, batch_size, glove, val_size, test_size):
+    # tokenization
+    tokenized_data = retrieve_data(data=data, glove=glove)
+    
+    #split data
+    train_data, val_data, test_data = split_data(tokenized_data, val_size, test_size)
+
+    train_sampler = SubsetRandomSampler(train_data)   
+    val_sampler = SubsetRandomSampler(val_data)
+    test_sampler = SubsetRandomSampler(test_data)
+
+    train_loader = DataLoader(tokenized_data, batch_size=batch_size, sampler=train_sampler)
+    val_loader = DataLoader(tokenized_data, batch_size=batch_size, sampler=val_sampler)
+    test_loader = DataLoader(tokenized_data, batch_size=batch_size, sampler=test_sampler)
+
+    return train_loader, val_loader, test_loader
 
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import torchtext
+import numpy as np
 
-from dataLoader import clean_data, tokenization
+from dataLoader import clean_data, getDataLoader, retrieve_data
+
+def train_pipeline(data, batch_size, glove, val_size, test_size):
+    train_loader, val_loader, test_loader = getDataLoader(data, batch_size, glove, val_size, test_size)
 
 
 if __name__ == "__main__":
-    # df = pd.read_csv("singapore_airlines_reviews.csv")
 
-    # Clean Data and save as data_clean.csv
+    # Reading + Cleaning Data (Using it for one-time use)
+    # Using GloVe pre-trained word embeddings
+    # Tokenizing Data
+
+
+    # df = pd.read_csv("singapore_airlines_reviews.csv")
     # clean_data(df)
 
-    df = pd.read_csv("data_clean.csv")
-    paragraphs = df['text']
-    
-    # Convert words into numerical representation using GloVe
-    GloVe = torchtext.vocab.GloVe(name="840B", dim=300)
 
-    tokenized_data = tokenization(paragraphs, glove=GloVe, lowercase=True, stop_words=True)
-    print(tokenized_data)
+    df = pd.read_csv("data_clean.csv")
+
+    GloVe = torchtext.vocab.GloVe(name="6B", dim=300)
+
+    train_pipeline(data=df, batch_size=256, glove=GloVe, val_size=0.35, test_size=0.1)
